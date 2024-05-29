017ee8453edac4204b1cc819cf4160526e2d4812
bathoryhead <marcdmartins@gmail.com>
2024-05-29 17:19:39 -0300

    ESCREVI AS 4 OPERAÇÕES EM PY.

---
+++ main.py
@@ -0,0 +1,29 @@
+num1 = float(input("digite o primeiro valor:"))
+num2 = float(input("digite o segunda valor:"))
+operacao = (input("escolha a operação: (+, -, *, /)"))
+
+#REALIZANDO As OPERAÇÕES
+if operacao == "+":
+    resultado = num1 + num2
+    print(f"o resultado da soma é:{resultado}")
+elif operacao == "-":
+    if num1 > num2:
+        resultado = num1 - num2
+        print(f"o resultado da subtração é:{resultado}")
+    else:
+        resultado = num2 - num1
+        print(f"o resultado da subtração é:{resultado}")
+elif operacao == "*":
+    resultado = num1 * num2
+    print(f"o resultado da multiplicação é:{resultado}")
+elif operacao == "/":
+    if num2 != 0:
+
+        if num1 > num2:
+            resultado = num1 / num2
+            print(f"o resultado da divisão é:{resultado}")
+        else:
+            resultado = num2 / num1
+            print(f"o resultado da divisão é:{resultado}")
+else:
+    print("operação inválida. Por favor escolha entre +, -, *, /")
