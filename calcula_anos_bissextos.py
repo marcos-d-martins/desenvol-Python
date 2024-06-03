c1a74a9c384d4fd570c958e1d20ed74b7047553d
bathoryhead <marcdmartins@gmail.com>
2024-06-02 23:07:49 -0300

    I'm  almost done

--- calcula_anos_bissextos.py
+++ calcula_anos_bissextos.py
@@ -2,32 +2,31 @@
 #, informando se é ou não um ano bissexto. 
 ano_informado = int(input("informe um ano bissexto: "))
 def calcula_Ano(anoInformado):
-    
-    if anoInformado % 400 == 0 or anoInformado % 4 == 0 and anoInformado % 100 != 0:
+    #  % = resultado do número dividido por 400 resultará ou não em Zero
+    #  % = resultado do número dividido por 4 resultará ou não em Zero
+    if anoInformado == '':
+        print("digite um valor! Não pode ser um campo vazio")
+    """if anoInformado.isnumeric():
+        print("O valor DEVE ser um número inteiro. Não pode conter letras")
+        print("\nAno digitado: ",anoInformado)"""
+    if anoInformado % 4 == 0:
         print("funciona")
+        print("\n ", anoInformado)
     else:
         print("erro. Ano não bissexto. Tente Novamente..")
-        while anoInformado % 400 != 0 or anoInformado % 4 != 0 and anoInformado % 100 != 0:
+        while anoInformado % 4 != 0:
             ano_informado = int(input("\ninforme um ano bissexto: "))
-        else:
+            if ano_informado % 4 == 0:
+                print("\nfunciona")
+                anos_errados = []
+                anos_errados.append(ano_informado)
+                print(anos_errados)
                 print("Parabéns! Ano bissexto encontrado.")
-                print("\nAno digitado: ",anoInformado)
+                print("\nAno digitado: ",ano_informado)
                 print("\nfim programa.")
-        """if anoInformado == '':
-            print("digite um valor! Não pode ser um campo vazio")
-            if not anoInformado.is_integer():
-                print("O valor DEVE ser um número inteiro. Não pode conter letras")
-                print("Parabéns! Ano bissexto encontrado.")
-                print("\nAno digitado: ",anoInformado)
-        
-    
+  
         #return false
-    else:
-        print(anoInformado," não é um ano bissexto. Tente de novo")
-        anos_digitados_nao_bissextos = []
-        anos_digitados_nao_bissextos.append(anoInformado)
-        anoInformado = int(input("informe um ano bissexto: "))
-        calcula_Ano(anoInformado)"""
+
 
 
 #if __name__ == calcula_Ano(ano_informado):
