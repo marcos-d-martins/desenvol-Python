df88a6cb4a7cc21de0133b37c97f1d9536b60197
bathoryhead <marcdmartins@gmail.com>
2024-06-02 23:34:25 -0300

    restam três ajustes a serem feitos: 1. verificar se o usuário não digitou valor algum (o CAMPO DEVE SER PREENCHIDO_ ),  2. se ele digitar LETRAS. O programa só deve aceitar números, números INTEIROS e 3. o programa deve exibir todos as tentativas de anos não-bissextos do usuário. Obrigado

--- calcula_anos_bissextos.py
+++ calcula_anos_bissextos.py
@@ -6,28 +6,24 @@ def calcula_Ano(anoInformado):
     #  % = resultado do número dividido por 4 resultará ou não em Zero
     if anoInformado == '':
         print("digite um valor! Não pode ser um campo vazio")
-    """if anoInformado.isnumeric():
-        print("O valor DEVE ser um número inteiro. Não pode conter letras")
-        print("\nAno digitado: ",anoInformado)"""
     if anoInformado % 4 == 0:
         print("funciona")
         print("\n ", anoInformado)
     else:
         print("erro. Ano não bissexto. Tente Novamente..")
         while anoInformado % 4 != 0:
-            ano_informado = int(input("\ninforme um ano bissexto: "))
-            if ano_informado % 4 == 0:
+            anoInformado = int(input("\ninforme um ano bissexto: "))
+            if anoInformado % 4 == 0:
                 print("\nfunciona")
-                anos_errados = []
-                anos_errados.append(ano_informado)
-                print(anos_errados)
                 print("Parabéns! Ano bissexto encontrado.")
-                print("\nAno digitado: ",ano_informado)
+                print("\nAno digitado: ",anoInformado)
                 print("\nfim programa.")
-  
-        #return false
-
-
-
+                break
+            else:
+                anos_errados = []
+                anos_errados.append(anoInformado)
+                print(anos_errados)
+                print("erro")
+        print("\n Anos não bissextos digitados: ",anos_errados)
 #if __name__ == calcula_Ano(ano_informado):
 calcula_Ano(ano_informado)
